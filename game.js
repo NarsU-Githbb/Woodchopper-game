fec1080da7872e782235dd654c4c2127bb8202fc
NarsU-Githbb <anssi.peltoniemi@hotmail.com>
2024-11-25 21:46:21 +0200

    Add Bot Choice Logic
    Made the bot choice logic with randomization between numbers 1-3.
    I also realized that the player's choice is not working without the periods (need to fix this)!
    
    These changes will make the game feel competitive & luck-based.

--- game.js
+++ game.js
@@ -7,27 +7,29 @@ let water = "3" || "3.";
 
 // Fuctions
 let answer; 
-let move = prompt("what would you choose? 1. Wood, 2. Axe or 3. Water (please give your answer in choise number)") 
-switch(move) {
+let userMove = prompt("what would you choose? 1. Wood, 2. Axe or 3. Water (please give your answer in choise number)") 
+let botMove = Math.floor(Math.random() * 3) + 1;
+switch(userMove, botMove) {
 
-case "1." || "1":
-text = "olet puu!"
+case "1" || "1.":
+window.alert("olet puu!")
 break;
 
-case "2." || "2":
-text ="very sharp!"
+case "2" || "2.":
+window.alert("very sharp!")
 break;
 
-case "3." || "3":
-text = "don't forget to drink some water"
+case "3" || "3.":
+window.alert("don't forget to drink some water")
 break; 
 
  
 default:
-text = "error you die";
+window.alert("error you die")
 
 }
 
-console.log(move)
+console.log(userMove)
+console.log(botMove)
 
 // Anything else
