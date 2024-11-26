ed22e5a85f6f69723c269210e53c0f3585d511dd
NarsU-Githbb <anssi.peltoniemi@hotmail.com>
2024-11-26 17:15:00 +0200

    Add Template Function for Game Score/Logic
    Made a template function that should dictate the game scoring & logic.
    (but currently the game logic is still dictated by numbers, when it should be dictated by the logic that I wrote).
    I'm thinking of changing the values with a, b, c instead of 1, 2, 3 (Maybe that's the answer?).
    So this commit is a great backup if things do go ape shit after I change the values!
    
    I also still need to find answers how to make the game loop from round to round & then stop/restart after someone gets
    3 points. Also how to keep up the score from round to round without the score deleting after a new round starts
    or how to delete scores completely after the game is done / new game begins.
    
    Lot of quetions, not so many answers!

--- game.js
+++ game.js
@@ -1,15 +1,17 @@
 // variables 
 
-let wood = "1" || "1.";
-let axe = "2" || "2.";
-let water = "3" || "3.";
+let userScr = 0;
+let botScr = 0;
+
+
+
 //   pelilogiikka on 1>3 2>1 3>2 (ei tarvi muuttaa numeroita sanoiksi)
 
-// Fuctions
-let answer; 
+
+ 
 let userMove = prompt("what would you choose? 1. Wood, 2. Axe or 3. Water (please give your answer in choise number)") 
-let botMove = Math.floor(Math.random() * 3) + 1;
-switch(userMove, botMove) {
+let botMove = Math.floor(Math.random() * 3) + 1 + "";
+switch(userMove) {
 
 case "1" || "1.":
 window.alert("olet puu!")
@@ -28,8 +30,39 @@ default:
 window.alert("error you die")
 
 }
+let wood = "1" || "1.";
+let axe = "2" || "2.";
+let water = "3" || "3.";
 
-console.log(userMove)
-console.log(botMove)
+gameLogic(userMove, botMove)
+
+// Fuctions
+
+function gameLogic() {
+
+wood > water;
+axe > wood;
+water > axe;
+// "1" = "1";
+// "2" = "2";
+// "3" = "3";
+
+if (userMove > botMove) {
+    return userScr = + 1 }
+if (botMove > userMove) {
+return botScr = + 1
+ } else { 
+ return (botMove = userMove);  
+ }
+} 
+
 
 // Anything else
+
+console.log(userMove);
+console.log(botMove)
+
+console.log(userScr)
+console.log(botScr)
+
+
