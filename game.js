b68f27f69ed64c42d3734f8a29d4e7404dcf69d3
NarsU <anssi.peltoniemi@hotmail.com>
2024-11-30 20:22:03 +0200

    Complete Woodchoppin' Game Javascript
    Made the game work on browser console! Waiting to re-visit the project again & make it more visual soon (in just couple of lessons in the Odin Project in fact).
    
    This was a great introduction project to Javascript for sure! My main takeaways being: 
    - Being conscious about how to copy code & not to self-blame myself to death by doing so (I feel like copying code is acceptable if I understand what the code is about & if I've actually tried to solve the problems myself first.)
    - Just trying something new out even if I haven't focused on it yet in the curriculum (stuff like loops).  
    - Don't F around with side branches yet or git might go berserk (I certainly need to figure out more how to use git).
    - I kinda feel bad that I resulted to use AI in the last portion of this project, since I really felt that I wanted to finish the project today & I was looping on the same problems for the last 2-3 days (I had 2 things that needed to be fixed in order to finish this project). I did understand what I was missing & why the fixes needed to be done, but damn..... really wanted to finish this project by my own! Luckily there's many more projects in the future so maybe I'll finish the next project completely by myself!

--- game.js
+++ game.js
@@ -1,82 +1,79 @@
-// variables 
+let gameIntro = alert("Let's play Woodchoppin' (the game of rock-paper-scissors on testosterones!)")
+let gameRules = alert("Rules: 1. Axe chops trees, but gets rusty in the water. 2. Wood consumes water, but won't stand a change in a fight against the axe 3.Water will make axe dull in the long run, but can't escape the thirst of the woods")
+let userScr = 0 
+let botScr = 0
 
-let userScr = 0;
-let botScr = 0;
+while (userScr < 3 && botScr < 3) { 
 
+userMove = prompt("which will you choose? Wood (then press 1)? Axe (then press 2)? or Water (press 3) (please give your answer in choice number without period)");
+botMove = Math.floor(Math.random() * 3) + 1 + "";
 
-
-//   pelilogiikka on 1>3 2>1 3>2 (ei tarvi muuttaa numeroita sanoiksi)
-
-let gameStart = alert("Let's play woodchoppin' (the game of rock-paper-scissors, but lame!) Rules: 1. Axe chops trees, but gets rusty in the water. 2. Wood consumes water, but won't stand a change in a fight against the axe 3.Water will make axe dull in the long run, but can't escape the thirst of the woods")
- 
-let userMove = prompt("what would you choose? 1. Wood, 2. Axe or 3. Water (please give your answer in choice number)")
-let botMove = Math.floor(Math.random() * 3) + 1 + "";
 
 switch(userMove) {
 
 case "1":
-window.alert("olet puu!")
-break;
+window.alert("Logs am I right!?!")
+break
 
 case "2":
-window.alert("very sharp!")
-break;
+window.alert("Very sharp!")
+break
 
 case "3":
-window.alert("don't forget to drink some water")
-break; 
+window.alert("Never forget to drink some water!")
+break 
 
- 
 default:
-window.alert("ERROR U DIE TRY AGAIN")
+window.alert("ERROR WRONG INPUT YOUR NOT A GOOD SPORT NOW U DIE")
+window.location.reload(true)
 
 }
 
-
-gameLogic()
-
-// Fuctions
-
-function gameLogic() {
-
-
 if (userMove == "1" && botMove == "3") {
-window.alert("You win! Your wood consumes the water. Here's a point for good luck!");
-return userScr = + 1 }
+window.alert("You win! Your wood consumes all the water. Here's a point for a little thirsty baby sapling like yourself!");
+userScr = userScr  + 1 }
 
-if (userMove == "2" && botMove == "1") {
-    window.alert("You win! Your Axe chopped the trees. Here's a point for good luck!");
-return userScr = + 1 }
+else if (userMove == "2" && botMove == "1") {
+    window.alert("You win! Your Axe chopped all the trees. Here's a point for being so brutal & edgy!");
+userScr = userScr + 1 }
 
-if (userMove == "3" && botMove == "2") {
-    window.alert("You win! Your water made the axe poopy. Here's a point for good luck!");
-return userScr = + 1 }
+else if (userMove == "3" && botMove == "2") {
+    window.alert("You win! Your water made the axe poopy. Here's a point for picking the boring choice!");
+userScr= userScr + 1 }
 
-if (botMove == "1" && userMove == "3") {
-    window.alert("You lose! Your water couldn't escape the thirst of the woods. Point for the computer master race!");
-return botScr = + 1 }
+else if (botMove == "1" && userMove == "3") {
+    window.alert("You lose! Your pathetic water couldn't even escape the thirst of the woods. Point for the computer master race (computers are scared of your water, but also think you're very boring)!");
+botScr = botScr + 1 }
 
-if (botMove == "2" && userMove == "1") {
-    window.alert("You lose! Your wood will make some excellent campfires for sure (thanks to axe). Point for the computer master race!");
-return botScr = + 1 }
+else if (botMove == "2" && userMove == "1") {
+    window.alert("You lose! Your wood will make some excellent campfires for sure. Point for the computer master race (they think computers might be more accepted if they were made out of wood too)!");
+botScr = botScr + 1 }
 
-if (botMove == "3" && userMove == "2") {
-    window.alert("You lose! You axe is all pooped thanks to the water! Point for the computer master race!");
-return botScr = + 1 }
+else if (botMove == "3" && userMove == "2") {
+    window.alert("You lose! Your axe is all pooped thanks to the water! Point for the computer master race (don't hit me says the computer)!");
+botScr = botScr + 1 }
 
- else { 
- return window.alert("draw! No points to be shared");  
+ else if (botMove == "2" && userMove == "2") { 
+ window.alert("Draw! These axes banging together will make any woodchopper sad.");
  }
-} 
+ else if (botMove == "1" && userMove == "1") { 
+    window.alert("Draw! The Tree is now branching like some bootleg version of Github (yuck no points, because that's plagiarism).");
+    }
+    else if (botMove == "3" && userMove == "3") { 
+        window.alert("Draw! Contestants drowned & that's boring!");
+        }        
+        window.alert(`Scores: You - ${userScr} | Bot - ${botScr}`)  
+    }
+    
+
+if(userScr === 3) {
+    window.alert("Congrats! You win & are ready for the Woodchoppin' job market!");
+    window.location.reload(true)
+    }
+   
+    if(botScr === 3) {
+    window.alert("What a Loser! Try harder next time you stick-snapping bozo!");
+     window.location.reload(true)
+    }
 
 
-// Anything else
-
-console.log(userMove);
-console.log(botMove)
-
-console.log(userScr)
-console.log(botScr)
-
-
-p
